a080d861450a9c172a834f3732b3000cb327014a
Royer Reyes León <royerleon7@gmail.com>
2022-07-27 16:03:47 -0500

    mirror false

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,4 +1,5 @@
-let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
+// let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
+let scanner = new Instascan.Scanner({ video: document.getElementById('preview'), mirror: false });
 	scanner.addListener('scan', function (content) {
 	console.log(content);
 	alert('CODIGO: '+ content);
